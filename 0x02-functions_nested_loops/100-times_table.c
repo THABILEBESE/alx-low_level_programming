3f4aa95e61fe24f214c9bf4bec0927a120a421b4
THABILEBESE <thabilebese@icloud.com>
2023-12-19 11:47:33 -0800

    number 16

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,23 @@
+
+#include "main.h"
+
+/**
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
+ */
+void print_times_table(int n)
+{
+if (n < 0 || n > 15)
+{
+return;
+}
+for (int i = 0; i <= n; ++i)
+{
+for (int j = 0; j <= n; ++j)
+{
+printf("%3d ", i * j);
+}
+printf("\n");
+}
+}
