86f45ce2990dfeed9313a513ff7deefe52021605
THABILEBESE <thabilebese@icloud.com>
2024-01-02 20:08:04 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,21 +14,22 @@ int _atoi(char *s)
 
 	while (s[c])
 	{
-		if (s[c] == 45)
-		{
-			min *= -1;
-		}
-		while (s[c] >= 48 && s[c] <= 57)
-		{
-			isis = 1;
-			ni = (ni * 10) + (s[c] - '0');
-			c++;
-		}
-		if (isis == 1)
-		{
-			break;
-		}
-		c++;
-		ni *= (ni);
+	if (s[c] == 45)
+	{
+	min *= -1;
 	}
+	while (s[c] >= 48 && s[c] <= 57)
+	{
+	isis = 1;
+	ni = (ni * 10) + (s[c] - '0');
+	c++;
+	}
+	if (isis == 1)
+	{
+	break;
+	}
+	c++;
+	}
+	ni *= (min);
+	return (ni);
 }
