89a18d0baf6c90ac1fd57da18cc2d49f626f2a2e
THABILEBESE <thabilebese@icloud.com>
2024-02-22 10:39:40 -0800

    update

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * clear_bit - sets the value of a given bit to 0
+ * @n: pointer to the number to change
+ * @index: index of the bit to clear
+ *
+ * Return: 1 for success, -1 for failure
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask;
+
+	mask = 1;
+	mask = mask << index;
+	if (index > sizeof(unsigned long int) * 8 || n == NULL)
+		return (-1);
+
+	if (((*n >> index) & 1) == 1)
+		*n = mask ^ *n;
+
+	return (1);
+}
