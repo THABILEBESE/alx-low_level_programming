df0d2f37b39bf2a580bd695d42e91710b6dd30c1
THABILEBESE <thabilebese@icloud.com>
2024-03-13 03:47:11 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,13 +4,13 @@
  * @head: pointer to the first node of the list
  * Return: no return
  */
-void freee_listint(listint_t *head)
+void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
 	while ((temp = head) != NULL)
 	{
 		head = head->next;
-		freee(temp);
+		free(temp);
 	}
 }
